551db2e84e5ec8d2c7c7fbd5dfd6a5ee2594fef3
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-03-10 11:04:37 +0330

    add timer

---
+++ ex1/src/App.js
@@ -0,0 +1,12 @@
+import './Assests/Styles/App.css';
+import Timer from './Components/Timer';
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
---
+++ ex1/src/Components/Timer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class Timer extends React.Component {
+constructor(props) {
+    super(props);
+
+    this.state = {
+        time : new Date(),
+        colors : ''
+    };
+}
+
+settime(){
+    this.setState({
+        time : new Date()
+    })
+}
+
+    componentDidMount(){
+        this.interval = setInterval(()=>
+        {
+            this.settime();
+            let randomColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+            this.setState({
+                colors : randomColor
+            })
+        }
+        ,1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1 style={{color : `${this.state.colors}`}}>{this.state.time.toLocaleTimeString()}</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Timer;
